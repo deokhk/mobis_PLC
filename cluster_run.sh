aa005c6f88d7d3f37d0ea0a070acc5ab66409c8e
deokhk <deokhk@postech.ac.kr>
2024-07-22 00:58:14 +0900

    Add cluster_run.sh

---
+++ cluster_run.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#SBATCH -J llama3_inst  # job name
+#SBATCH -o sbatch_output_log/output_%x_%j.out # standard output and error log
+#SBATCH -p RTX6000ADA  # queue name or partiton name
+#SBATCH -t 72:00:00 # Run time (hh:mm:ss)
+#SBATCH  --gres=gpu:2
+#SBATCH  --nodes=1
+#SBATCH  --ntasks=2
+#SBATCH  --cpus-per-task=4
+srun -l /bin/hostname
+srun -l /bin/pwd
+srun -l /bin/date
+
+module purge 
+ml load ohpc
+
+date
+bash /home1/deokhk_1/project/mobis_PLC/train_llama3_instruct.sh
+
+date
