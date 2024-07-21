227771cce322c865aa8234c9559cb40c5093635b
deokhk <deokhk@postech.ac.kr>
2024-07-22 01:33:08 +0900

    Korean script modified

--- train_llama3_inst_korean.sh
+++ train_llama3_inst_korean.sh
@@ -12,7 +12,7 @@ PER_DEVICE_EVAL_BATCH_SIZE=1
 NUM_EPOCHS=15
 WANDB_RUN_NAME=mobis_llama3_korean_instruct
 
-CUDA_VISIBLE_DEVICES=2,3 torchrun --nproc_per_node=$NUM_GPUS --nnodes 1 --rdzv_backend c10d --rdzv_endpoint localhost:0 train.py \
+torchrun --nproc_per_node=$NUM_GPUS --nnodes 1 --rdzv_backend c10d --rdzv_endpoint localhost:0 train.py \
 --output_dir $OUTPUT_DIR \
 --model_id $MODEL_ID \
 --train_data $TRAIN_DATA \
